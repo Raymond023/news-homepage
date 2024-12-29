83c900f02a89250d0bba66c8b2cdd22809f24535
Raymond023 <raymondraphael11@gmail.com>
2024-12-29 23:50:03 +0100

    added media queries for larger screens & added side navigation for mobile and tablet sized screens

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const openNav = document.querySelector('.open-nav');
+const closeNav = document.querySelector('.close-nav');
+const navBar = document.querySelector('nav');
+const overlay = document.querySelector('.overlay');
+
+openNav.addEventListener('click', () => {
+    overlay.classList.add('show-overlay');
+    navBar.classList.add('show');
+});
+
+closeNav.addEventListener('click', () => {
+    overlay.classList.remove('show-overlay');
+    navBar.classList.remove('show');
+});
