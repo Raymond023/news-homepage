606bdc620b89d5aac9d42d2ed124c2900a698b81
Raymond023 <raymondraphael11@gmail.com>
2025-01-05 23:35:47 +0100

    changed html layout and css

--- script.js
+++ script.js
@@ -3,14 +3,15 @@
 const openNav = document.querySelector('.open-nav');
 const closeNav = document.querySelector('.close-nav');
 const navBar = document.querySelector('nav');
-const overlay = document.querySelector('.overlay');
+const navDialog = document.querySelector('#nav-dialog')
+const body = document.body;
 
 openNav.addEventListener('click', () => {
-    overlay.classList.add('show-overlay');
-    navBar.classList.add('show');
+    body.classList.toggle('overlay');
+    navDialog.showModal();
 });
 
 closeNav.addEventListener('click', () => {
-    overlay.classList.remove('show-overlay');
-    navBar.classList.remove('show');
+    body.classList.toggle('overlay');
+    navDialog.close();
 });
